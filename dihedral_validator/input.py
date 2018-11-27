c8c54711e07e4c4571d2eca67cf2c7f91130fac1
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-11-27 15:15:31 +0100

    simple input

---
+++ dihedral_validator/input.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import yaml
+
+def read_input_file(path:str):
+    with open(path, 'r') as f:
+        return yaml.load(f)
+
+
+if __name__ == '__main__':
+    import os
+    print(read_input_file(os.path.join('example', 'input')))
