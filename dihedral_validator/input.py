2676be9d72060be7ec1dd9f84ffc9891dcb6024b
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-11-28 08:17:33 +0100

    reformated

--- dihedral_validator/input.py
+++ dihedral_validator/input.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import yaml
 
-def read_input_file(path:str):
+
+def read_input_file(path: str):
     with open(path, 'r') as f:
         return yaml.load(f)
 
 
 if __name__ == '__main__':
     import os
+
     print(read_input_file(os.path.join('example', 'input')))
