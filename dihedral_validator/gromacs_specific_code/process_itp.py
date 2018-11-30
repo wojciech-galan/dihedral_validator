0a7b91d60201901a5e60eef4e962c91e4ab7be3b
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-11-30 11:54:56 +0100

    itp creation works

--- dihedral_validator/gromacs_specific_code/process_itp.py
+++ dihedral_validator/gromacs_specific_code/process_itp.py
@@ -16,13 +16,24 @@ COMMENT_RE = re.compile('(^;.*\n)+', re.MULTILINE)
 
 
 def prepare_itp_file(in_ipt: str, new_params: dict, params_type: int, out_itp: str,
-                     comment_substitution: Dict[str, str]):
+                     comment_substitution: Dict[str, str], general_comments='', newline='\n'):
     defines, sections_dict = parse_itp_file(in_ipt)
-    for k, v in sections_dict:
-        if filter_out_comments.get(k, False):
-            sections_dict[k] = remove_lines_containing_comments[v]
+    out_list = [newline.join(defines)]
+    out_list.append(newline.join(['; ' + s for s in general_comments.split(newline)]))
+    out_list.append(create_dihedraltypes(new_params, params_type,
+                                         additional_comment_string=comment_substitution.get('dihedraltypes',
+                                                                                            None) or ';'))
+    for k in ['moleculetype', 'atoms', 'bonds', 'angles', 'dihedrals1', 'pairs', 'dihedrals2']:
+        if k in comment_substitution:
+            out_list.append(substitute_comment(sections_dict[k], comment_substitution[k]))
+        else:
+            out_list.append(sections_dict[k])
+    with open(out_itp, 'w') as f:
+        f.write((newline + newline).join(out_list))
 
-def substitute_comment(section_string:str, new_comment:str, comment_re:Pattern=COMMENT_RE, newline:str='\n') -> str:
+
+def substitute_comment(section_string: str, new_comment: str, comment_re: Pattern = COMMENT_RE,
+                       newline: str = '\n') -> str:
     '''
     Substitutes comments in section_string with new_comment
     :param section_string:
@@ -32,16 +43,20 @@ def substitute_comment(section_string:str, new_comment:str, comment_re:Pattern=C
     replacement_string = newline.join(['; ' + s for s in new_comment.split(newline)]) + newline
     return comment_re.sub(replacement_string, section_string)
 
-def create_dihedraltypes(params, base_comment_string:str =';  i    j    k    l   func     coefficients', additional_comment_string=';', newline='\n'):
+
+def create_dihedraltypes(params, params_type: int,
+                         base_comment_string: str = ';  i    j    k    l   func     coefficients',
+                         additional_comment_string=';', newline='\n'):
     s = newline.join(['[ dihedraltypes ]', base_comment_string, additional_comment_string]) + newline
     for k, v in params.items():
-        v = v + [0 for i in range(6-len(v))]
+        v = v + [0 for i in range(6 - len(v))]
         print(k, adapt_input_dihedral_types_to_gromacs_format(k))
-        s+= '{} {:9.4f} {:9.4f} {:9.4f} {:9.4f} {:9.4f} {:9.4f}{}'.format(adapt_input_dihedral_types_to_gromacs_format(k), *v, newline)
+        s += '{}   {}  {:9.4f} {:9.4f} {:9.4f} {:9.4f} {:9.4f} {:9.4f}{}'.format(
+            adapt_input_dihedral_types_to_gromacs_format(k), params_type, *v, newline)
     return s
 
 
-def adapt_input_dihedral_types_to_gromacs_format(input_dihedral:str) -> str:
+def adapt_input_dihedral_types_to_gromacs_format(input_dihedral: str) -> str:
     # example input: CT-CT-CT-OS
     # example output: CT    CT   CT   OS
     a, b, c, d = input_dihedral.split('-')
@@ -53,7 +68,7 @@ def parse_itp_file(in_file: str, newline='\n', section_re_template: str = SECTIO
     with open(in_file, 'r') as f:
         content = f.read()
     defines = re.findall(define_re, content)
-    #dihedraltypes = find_section('dihedraltypes', content, section_re_template=section_re_template)
+    # dihedraltypes = find_section('dihedraltypes', content, section_re_template=section_re_template)
     moleculetype = find_section('moleculetype', content, section_re_template=section_re_template)
     atoms = find_section('atoms', content, section_re_template=section_re_template)
     bonds = find_section('bonds', content, section_re_template=section_re_template)
@@ -89,13 +104,9 @@ def find_section(section_name: str, string_to_be_searched, section_re_template: 
 
 if __name__ == '__main__':
     from dihedral_validator.input import read_input_file
+
     params = read_input_file(os.path.join('example', 'input'))
     print(params)
-    created_dihedraltypes = create_dihedraltypes({'CT-CT-CT-OS': [1, 2, 3, 4], 'CT-CT-OS-CT': [2, 3, 4, 5, 6, 7]})
-    print('------------------------------')
-    print(created_dihedraltypes)
-    print('------------------------------')
-    print(substitute_comment(created_dihedraltypes, 'huj dupa\nkurwa cipa'))
-    #print(parse_itp_file(os.path.join('example', 'gromacs_specific_files', 'triacetin_qqAWA_q1_new_t3t4.itp'), {}))
-    # TODO program i data puszczania tej analizy w dihedraltypes + dodatki (ładunki czy coś) - czyli zrobienie dihedraltypes od zera
-    # TODO ale program jest przez jakiś wyższy skrypt ustawiany
+    prepare_itp_file(os.path.join('example', 'gromacs_specific_files', 'triacetin_qqAWA_q1_new_t3t4.itp'), params, 3,
+                     os.path.join('temp', 'out_itp'), {'moleculetype':'zmieniłem moleculetype'}, 'nowy komentarz ogólny\ni jego druga linia')
+
