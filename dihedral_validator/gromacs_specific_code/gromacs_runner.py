c716bc2fa4cbc8e1e3ec0692fa13fa07b729b87c
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-01 09:17:41 +0100

    blah

--- dihedral_validator/gromacs_specific_code/gromacs_runner.py
+++ dihedral_validator/gromacs_specific_code/gromacs_runner.py
@@ -1,2 +1,17 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
+import subprocess
+import os
+
+class Wrapper():
+    def __init__(self):
+        super().__init__()
+
+    def run(self, command):
+        subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+
+
+if __name__ == '__main__':
+    w = Wrapper()
+    w.run('grompp -f example/gromacs_specific_files/gas_10ns.mdp -p example/gromacs_specific_files/gas_10ns-qqAWA_q1_new_t3t4.top -c example/gromacs_specific_files/md-gas_10ns-qqAWA_q1.gro -o temp/md-gas.tpr -maxwarn 11')
