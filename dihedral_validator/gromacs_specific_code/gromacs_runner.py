98a22305eaaf0893fa9b0abe4ee1c4467652e65b
wojciech-galan <wojciech.galan@gmail.com>
2018-12-03 07:40:57 +0100

    mdrun command

--- dihedral_validator/gromacs_specific_code/gromacs_runner.py
+++ dihedral_validator/gromacs_specific_code/gromacs_runner.py
@@ -15,3 +15,5 @@ class Wrapper():
 if __name__ == '__main__':
     w = Wrapper()
     w.run('grompp -f example/gromacs_specific_files/gas_10ns.mdp -p example/gromacs_specific_files/gas_10ns-qqAWA_q1_new_t3t4.top -c example/gromacs_specific_files/md-gas_10ns-qqAWA_q1.gro -o temp/md-gas.tpr -maxwarn 11')
+    print('\n\n\n\n\n-----------------------------w---------------------\n\n\n')
+    w.run('mdrun -deffnm temp/md-gas')
