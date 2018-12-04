88ceac049b8df5b09b06a3f5ad2e0a8f7f19ae69
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-04 12:23:34 +0100

    computes potential

--- dihedral_validator/gromacs_specific_code/energy_computation.py
+++ dihedral_validator/gromacs_specific_code/energy_computation.py
@@ -3,8 +3,22 @@
 
 import subprocess
 import os
+import tempfile
 
-def validate_path(path:str):
+
+def compute_potential(edr_file_path: str, choice: int = 9, g_energy_command: str = 'gmx energy'):
+    # validate user input
+    validate_path(edr_file_path)
+    validate_number(choice)
+    truncated_edr_file_path = edr_file_path.replace('.edr', '')
+    with tempfile.NamedTemporaryFile() as outfile:
+        p = subprocess.Popen('{} -f {} -o {}'.format(g_energy_command, truncated_edr_file_path, outfile.name), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='ascii')
+        out, err = p.communicate('9/n/n')
+        # TODO check whether energy is also in out for gromacs4
+        return extract_energy_from_genergy_output(out)
+
+
+def validate_path(path: str):
     '''
     Raises FileNotFoundError when path not exists
     :param path:
@@ -13,7 +27,8 @@ def validate_path(path:str):
     with open(path, 'r'):
         pass
 
-def validate_number(number:int):
+
+def validate_number(number: int):
     '''
     Raises RuntimeErrorm when number is not int
     :param number:
@@ -23,8 +38,20 @@ def validate_number(number:int):
         raise RuntimeError('Argument should be integer')
 
 
+def extract_energy_from_genergy_output(genergy_output:str) -> float:
+    '''
+    Extracts interesting parts of string similar to:
+    """Energy                      Average   Err.Est.       RMSD  Tot-Drift
+    -------------------------------------------------------------------------------
+    Potential                  -154.457       0.43    16.2399   0.167619  (kJ/mol)"""
+    :param genergy_output:
+    :return:
+    '''
+    pass
+
 
 if __name__ == '__main__':
     # validate_path('kupsko')
     # validate_number('16')
-    compute_potential_gas()
+    print(os.listdir('.'))
+    compute_potential('example/gromacs_specific_files/md-gas.edr')
