47fff5c58e54157738344e46ea75822e9044bbac
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-04 17:37:49 +0100

    computes free energy

--- dihedral_validator/gromacs_specific_code/energy_computation.py
+++ dihedral_validator/gromacs_specific_code/energy_computation.py
@@ -9,13 +9,19 @@ import re
 EXTRACT_OUTPUT_RE = '-+\n{} +([-+]?\d+.\d)'
 
 
-def compute_potential(edr_file_path: str, choice: int, choice_string: str, g_energy_command: str = 'gmx energy'):
+def compute_free_energy(e_pot_liq: float, e_pot_gas: float, temperature: float, num_of_particles: float,
+                        R_CONST: float = 0.0083144598):
+    return e_pot_liq/num_of_particles - e_pot_gas + R_CONST*temperature # result in kJ/mol
+
+def extract_from_edr_file(edr_file_path: str, choice: int, choice_string: str, g_energy_command: str = 'gmx energy'):
     # validate user input
     validate_path(edr_file_path)
     validate_number(choice)
     truncated_edr_file_path = edr_file_path.replace('.edr', '')
     with tempfile.NamedTemporaryFile() as outfile:
-        p = subprocess.Popen('{} -f {} -o {}'.format(g_energy_command, truncated_edr_file_path, outfile.name), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='ascii')
+        p = subprocess.Popen('{} -f {} -o {}'.format(g_energy_command, truncated_edr_file_path, outfile.name),
+                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                             encoding='ascii')
         out, err = p.communicate('{:d}/n/n'.format(choice))
         # TODO check whether energy is also in out for gromacs4
         return extract_values_from_genergy_output(out, choice_string)
@@ -41,7 +47,8 @@ def validate_number(number: int):
         raise RuntimeError('Argument should be integer')
 
 
-def extract_values_from_genergy_output(genergy_output:str, what_to_extract:str, re_to_capture_output:str = EXTRACT_OUTPUT_RE) -> float:
+def extract_values_from_genergy_output(genergy_output: str, what_to_extract: str,
+                                       re_to_capture_output: str = EXTRACT_OUTPUT_RE) -> float:
     '''
     Extracts interesting parts of string similar to:
     """Energy                      Average   Err.Est.       RMSD  Tot-Drift
@@ -53,11 +60,12 @@ def extract_values_from_genergy_output(genergy_output:str, what_to_extract:str, 
     formatted_re = re_to_capture_output.format(what_to_extract)
     return float(re.search(formatted_re, genergy_output).group(1))
 
-# todo do the same for liquid and density
 
 if __name__ == '__main__':
     # validate_path('kupsko')
     # validate_number('16')
-    print(os.listdir('.'))
-    print(compute_potential('example/gromacs_specific_files/md-gas.edr', 9, 'Potential'))
-    print(compute_potential('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 20, 'Density'))
+    # print(extract_from_edr_file('example/gromacs_specific_files/md-gas.edr', 9, 'Potential'))
+    # print(extract_from_edr_file('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 20, 'Density'))
+    gas_potential = extract_from_edr_file('example/gromacs_specific_files/md-gas.edr', 9, 'Potential')
+    liquid_potential = extract_from_edr_file('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 10, 'Potential')
+    print(compute_free_energy(liquid_potential, gas_potential, 280, 100))
