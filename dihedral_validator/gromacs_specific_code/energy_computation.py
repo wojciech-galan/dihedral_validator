6b33d6db50747c0e42424c488dcc06980c7058f4
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-04 13:06:45 +0100

    extracts potential as well as density

--- dihedral_validator/gromacs_specific_code/energy_computation.py
+++ dihedral_validator/gromacs_specific_code/energy_computation.py
@@ -6,19 +6,19 @@ import os
 import tempfile
 import re
 
-EXTRACT_OUTPUT_RE = '-+\nPotential +([-+]?\d+.\d)'
+EXTRACT_OUTPUT_RE = '-+\n{} +([-+]?\d+.\d)'
 
 
-def compute_potential(edr_file_path: str, choice: int = 9, g_energy_command: str = 'gmx energy'):
+def compute_potential(edr_file_path: str, choice: int, choice_string: str, g_energy_command: str = 'gmx energy'):
     # validate user input
     validate_path(edr_file_path)
     validate_number(choice)
     truncated_edr_file_path = edr_file_path.replace('.edr', '')
     with tempfile.NamedTemporaryFile() as outfile:
         p = subprocess.Popen('{} -f {} -o {}'.format(g_energy_command, truncated_edr_file_path, outfile.name), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='ascii')
-        out, err = p.communicate('9/n/n')
+        out, err = p.communicate('{:d}/n/n'.format(choice))
         # TODO check whether energy is also in out for gromacs4
-        return extract_energy_from_genergy_output(out)
+        return extract_values_from_genergy_output(out, choice_string)
 
 
 def validate_path(path: str):
@@ -41,7 +41,7 @@ def validate_number(number: int):
         raise RuntimeError('Argument should be integer')
 
 
-def extract_energy_from_genergy_output(genergy_output:str, re_to_capture_output:str = EXTRACT_OUTPUT_RE) -> float:
+def extract_values_from_genergy_output(genergy_output:str, what_to_extract:str, re_to_capture_output:str = EXTRACT_OUTPUT_RE) -> float:
     '''
     Extracts interesting parts of string similar to:
     """Energy                      Average   Err.Est.       RMSD  Tot-Drift
@@ -50,7 +50,8 @@ def extract_energy_from_genergy_output(genergy_output:str, re_to_capture_output:
     :param genergy_output:
     :return:
     '''
-    return float(re.search(re_to_capture_output, genergy_output).group(1))
+    formatted_re = re_to_capture_output.format(what_to_extract)
+    return float(re.search(formatted_re, genergy_output).group(1))
 
 # todo do the same for liquid and density
 
@@ -58,4 +59,5 @@ if __name__ == '__main__':
     # validate_path('kupsko')
     # validate_number('16')
     print(os.listdir('.'))
-    print(compute_potential('example/gromacs_specific_files/md-gas.edr'))
+    print(compute_potential('example/gromacs_specific_files/md-gas.edr', 9, 'Potential'))
+    print(compute_potential('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 20, 'Density'))
