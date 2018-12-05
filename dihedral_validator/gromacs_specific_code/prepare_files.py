a87fc1e28e3c66ddf24827c6e94b66b775d2459e
wojciech-galan <wojciech.galan@gmail.com>
2018-12-05 03:22:04 +0100

    prepare_files should work OK

--- dihedral_validator/gromacs_specific_code/prepare_files.py
+++ dihedral_validator/gromacs_specific_code/prepare_files.py
@@ -7,6 +7,8 @@ from dihedral_validator.gromacs_specific_code.process_itp import prepare_itp_fil
 
 
 def prepare_files(itp_template_path: str, itp_out_path: str, new_params: Dict, params_type: int,
-                  ipt_comment_substitution: Dict[str, str], top_path: str, molecules: Dict[str:int], system_line: str,
-                  forcefield_itp_path: str = 'oplsaa.ff/forcefield.itp', newline: str = '\n'):
-    pass
+                  ipt_comment_substitution: Dict[str, str], ipt_general_comments: str, top_path: str,
+                  molecules: Dict[str:int], system_line: str, forcefield_itp_path: str = 'oplsaa.ff/forcefield.itp',
+                  newline: str = '\n'):
+    prepare_itp_file(itp_template_path, new_params, params_type, itp_out_path, ipt_comment_substitution, ipt_general_comments, newline)
+    create_top_file(itp_out_path, molecules, system_line, top_path, forcefield_itp_path, newline)
