ce3cbe36c7cce869f863f6eaea2cfc4c66df5848
wojciech-galan <wojciech.galan@gmail.com>
2018-12-06 07:30:46 +0100

    moved code from prepare_files

--- dihedral_validator/gromacs_specific_code/gromacs_pipeline.py
+++ dihedral_validator/gromacs_specific_code/gromacs_pipeline.py
@@ -4,7 +4,8 @@
 from typing import Dict
 import os
 import time
-from dihedral_validator.gromacs_specific_code import prepare_files
+from dihedral_validator.gromacs_specific_code.create_top import create_top_file
+from dihedral_validator.gromacs_specific_code.process_itp import prepare_itp_file
 from dihedral_validator.input import read_input_file
 
 
@@ -20,3 +21,6 @@ def gromacs_pipeline(itp_template_path: str, itp_out_path: str, new_params_path:
     ipt_general_comments += '{}This file was created on {}{} based on template {} modified on{}{} and params {} modified on {}'.format(
         newline, time_now, newline, itp_template_abspath, itp_template_modification_time, newline, params_abspath,
         params_modification_time)
+    prepare_itp_file(itp_template_path, new_params, params_type, itp_out_path, ipt_comment_substitution,
+                     ipt_general_comments, newline)
+    create_top_file(itp_out_path, molecules, system_line, top_path, forcefield_itp_path, newline)
