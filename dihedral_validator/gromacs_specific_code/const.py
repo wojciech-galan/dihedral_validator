cf0407a5ef8847d2773f325323c4d3df9330aa64
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-06 13:04:04 +0100

    intial code

---
+++ dihedral_validator/gromacs_specific_code/const.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+grompp_dict = {'4': 'grompp', '2018': 'gmx grompp'}
+mdrun_dict = {'4': 'mdrun', '2018': 'gmx mdrun'}
+genergy_dict = {'4': 'genergy', '2018': 'gmx energy'}
