1ab7c48af1a4f15eb964fc93d02be04d1b313175
wojciech-galan <wojciech.galan@gmail.com>
2018-12-07 19:31:14 +0100

    works well with gromacs 4

--- dihedral_validator/gromacs_specific_code/energy_computation.py
+++ dihedral_validator/gromacs_specific_code/energy_computation.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import os
 import tempfile
 import re
+from dihedral_validator.gromacs_specific_code.const import genergy_dict
 
 EXTRACT_OUTPUT_RE = '-+\n{} +([-+]?\d+.\d)'
 
@@ -23,18 +23,25 @@ def compute_free_energy(e_pot_liq: float, e_pot_gas: float, temperature: float, 
     return (e_pot_liq / num_of_particles - e_pot_gas + R_CONST * temperature) / 4.1868  # result in kcal/mol
 
 
-def extract_from_edr_file(edr_file_path: str, choice: int, choice_string: str, g_energy_command: str = 'gmx energy'):
+def extract_from_edr_file(edr_file_path: str, choice: int, choice_string: str, gromacs_version=None):
     # validate user input
     validate_path(edr_file_path)
     validate_number(choice)
     truncated_edr_file_path = edr_file_path.replace('.edr', '')
     with tempfile.NamedTemporaryFile() as outfile:
-        p = subprocess.Popen('{} -f {} -o {}'.format(g_energy_command, truncated_edr_file_path, outfile.name),
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
-                             encoding='ascii')
-        out, err = p.communicate('{:d}/n/n'.format(choice))
-        # TODO check whether energy is also in out for gromacs4
-        return extract_values_from_genergy_output(out, choice_string)
+        print(gromacs_version)
+        if gromacs_version == '2018':
+            p = subprocess.Popen(
+                '{} -f {} -o {}'.format(genergy_dict[gromacs_version], truncated_edr_file_path, outfile.name),
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='utf-8')
+            out, err = p.communicate('{:d}/n/n'.format(choice))
+            return extract_values_from_genergy_output(out, choice_string)
+        elif gromacs_version == '4':
+            p = subprocess.Popen(
+                '{} -f {} -o {}'.format(genergy_dict[gromacs_version], truncated_edr_file_path, outfile.name),
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            out, err = p.communicate('{:d}/n/n'.format(choice).encode())
+            return extract_values_from_genergy_output(out.decode(), choice_string)
 
 
 def validate_path(path: str):
@@ -76,7 +83,7 @@ if __name__ == '__main__':
     # validate_number('16')
     # print(extract_from_edr_file('example/gromacs_specific_files/md-gas.edr', 9, 'Potential'))
     # print(extract_from_edr_file('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 20, 'Density'))
-    gas_potential = extract_from_edr_file('example/gromacs_specific_files/md-gas.edr', 9, 'Potential')
+    gas_potential = extract_from_edr_file('example/gromacs_specific_files/md-gas.edr', 9, 'Potential', '4')
     liquid_potential = extract_from_edr_file('example/gromacs_specific_files/md-liquid_10nsRB_bestHC.edr', 10,
-                                             'Potential')
+                                             'Potential', '4')
     print(compute_free_energy(liquid_potential, gas_potential, 280, 100))
