bccf1600564c3217c77d97fc007c7f693a1e5aff
wojciech-galan <wojciech.galan@uj.edu.pl>
2018-12-11 10:50:22 +0100

    tpr file extension dict

--- dihedral_validator/gromacs_specific_code/const.py
+++ dihedral_validator/gromacs_specific_code/const.py
@@ -4,3 +4,4 @@
 grompp_dict = {'4': 'grompp', '2018': 'gmx grompp'}
 mdrun_dict = {'4': 'mdrun', '2018': 'gmx mdrun'}
 genergy_dict = {'4': 'g_energy', '2018': 'gmx energy'}
+tpr_file_extension = {'4': '', '2018': '.tpr'}
